46ba7dfa3b5c26bc57ff5319d30883702e781f78
Artemis <art.lazanaki@gmail.com>
2022-12-20 15:22:03 +0200

    Mpla mpla sthn arxh kai liga comments

--- game.cpp
+++ game.cpp
@@ -30,28 +30,37 @@ void set_player_preferences(int& dim_x, int& dim_y, char& team) {
 		try {
 			string x;
 			string y;
-			cout << "Enter the dimensions of the map: " << endl;
+			cout << "The dimensions must be from 10 to 30" << endl;
+			cout << "Please enter the dimensions of the map: " << endl;
 			cin >> x;
 			cin >> y;
 	
-			if (x.length() > 3 || y.length() > 3) {
-				throw(ERROR_CHAR_INPUT);
+			if (x.length() >= 3 || y.length() >= 3) {		// If the input has more than 3 digits/characters is surely wrong
+				throw(ERROR_CHAR_INPUT);					// in that way an overflow is avoided
 			}
-			for (int i = 0; i < x.length(); i++) {
-				if (!isdigit(x[i])) {
+
+			/* If the input contains a character that's not represent a digit is wrong */ 
+			for(int i = 0; i < x.length() ; i++){
+				if (!isdigit(x[i])) {						
 					throw(ERROR_CHAR_INPUT);
 				}
 			}
 			for (int i = 0; i < y.length(); i++) {
-				if (!isdigit(y[i])) {
+				if (!isdigit(y[i])) {						
 					throw(ERROR_CHAR_INPUT);
 				}
 			}
+			/*if the input is 1 or 2 characters that represents a number convert the string to number*/
 			dim_x = stoi(x);
 			dim_y = stoi(y);
+
+			// by agreement number of werewolves and vampires at the start is x*y/30
+			// and we want at least 3 fighters for each team
+			// also grids that has dimensions over 30 don't fit well at the terminal
 			if (dim_x < 10 || dim_y < 10 || dim_x > 30 || dim_y > 30) {
 				throw(ERROR_OUT_OF_RANGE);
 			}
+			/*if the input is valid ask the player to choose his/her team*/
 			repeat = false;
 		}
 
@@ -59,23 +68,26 @@ void set_player_preferences(int& dim_x, int& dim_y, char& team) {
 			switch (n) {
 			case(ERROR_OUT_OF_RANGE):
 				cout << "Dimensions out of range...\n"
-					<< "Reminder x and y must from 10 to 30 "
-					<< "Please try again.\n";
+					 << "Reminder x and y must from 10 to 30 "
+					 << "Please try again.\n";
 				break;
 
 			case(ERROR_CHAR_INPUT):
 				cout << "Characters are not allowed...\n"
-					<< "Please try again.\n";
+					 << "Please try again.\n";
 				break;
 			}
 			repeat = true;
 		}
 	}
+
+	/* if the char entered for team is different from 'V' or 'W', the input is not valid */
+	// repeat until player enter a valid input 
 	repeat = true;
 	while (repeat) {
 		try {
 			cout << "Time to choose your team...\n"
-				<< "Enter V for Vampires and W for Werewolves: \n";
+				 << "Enter V for Vampires and W for Werewolves: \n";
 			cin >> team;
 
 			if (team != 'V' && team != 'W') {
@@ -93,10 +105,27 @@ void set_player_preferences(int& dim_x, int& dim_y, char& team) {
 
 /*Game - Member functions & Constructor*/
 game::game(const int& x, const int& y, const char& team) : active(true), Map(x, y, team), Player(team), Avatar(x / 2 + 1, y/ 2 + 1, team), team_vampires(Map), team_werewolves(Map), Magic_filter(Map), winners_team('0') {
-	std::cout << "The game is starting..." << endl
-		<< "You are able to move in the map using arrow keys ..." << endl
-		<< "Press space to pause game and X to exit..." << endl
-		<< "Enjoy!\n\n";
+	cout << "			The game is starting...\n\n";
+	Sleep(1000);
+	cout << "			During the game, vampires and werewolves are fighting...\n\n";
+	Sleep(1500);
+	cout << "			You are able to move in the map using arrow keys ...\n\n";
+	Sleep(1500);
+	cout << "			Your job is to collect as many magic filters as you can,\n" 
+			"			in order to heal the team you chose to support!\n\n";
+	Sleep(2000);
+	cout <<  "			At least 10 magic filters are required. Press 'F' to use them.\n\n";
+	Sleep(2000);
+	if (team == 'V')
+		cout << "			But remember... You can only use them at day...\n\n";
+	else
+		cout << "			But remember... You can only use them at night...\n\n";
+	Sleep(2000);
+	cout << "			Press space to pause and see more info about the game\n\n";
+	Sleep(1500);
+	cout << "			Press 'X' to exit...\n\n";
+	Sleep(1000);
+	cout << "			Enjoy!\n\n";
 
 	map_entity* p = &Avatar;
 	Map.place_to_grid(Avatar.get_i(), Avatar.get_j(), p);
